e76c52d945935a6e1fc424ec8332abca5a1001da
Martin  Infante <m.infante@MBP-256gb-8gb-Brown.local>
2024-07-03 16:12:29 -0300

    Add gitignore

--- main.py
+++ main.py
@@ -1,44 +1,116 @@
 import os
 import secrets
 
-from fastapi import Depends, FastAPI, File, HTTPException, UploadFile, status
-from fastapi.responses import FileResponse
-from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import matplotlib
+from fastapi import Depends, FastAPI, File, Request, UploadFile
+from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi_login import LoginManager
+from fastapi_login.exceptions import InvalidCredentialsException
+from starlette.responses import RedirectResponse
 
-from process_csv import process_csv
+matplotlib.use("pdf")
 
 app = FastAPI()
 
-security = HTTPBasic()
+
+class NotAuthenticatedException(Exception):
+    pass
+
+
+# Secret key for session management
+with open("secret.txt", "r") as f:
+    SECRET = f.read()
 
 # Read username and password from a local file
 with open("auth.txt", "r") as f:
     AUTH_USERNAME, AUTH_PASSWORD = f.read().strip().split(":")
 
-
-def get_current_user(credentials: HTTPBasicCredentials = Depends(security)):
-    correct_username = secrets.compare_digest(credentials.username, AUTH_USERNAME)
-    correct_password = secrets.compare_digest(credentials.password, AUTH_PASSWORD)
-    if not (correct_username and correct_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Basic"},
-        )
-    return credentials.username
+# Basic authentication security
+manager = LoginManager(
+    SECRET,
+    "/login",
+    use_cookie=True,
+    not_authenticated_exception=NotAuthenticatedException,
+)
 
 
+# Login route
+@app.post("/login/")
+async def login(data: OAuth2PasswordRequestForm = Depends()):
+    if data.username == AUTH_USERNAME and secrets.compare_digest(
+        data.password, AUTH_PASSWORD
+    ):
+        # Correct credentials, set session or cookie here if needed
+        token = manager.create_access_token(data={"sub": data.username})
+        response = RedirectResponse(url="/", status_code=302)
+        manager.set_cookie(response, token)
+        return response
+    else:
+        raise InvalidCredentialsException
+
+
+@manager.user_loader()
+def load_user(user_id: str):
+    return AUTH_USERNAME
+
+
+# Protected endpoint example
 @app.post("/uploadfile/")
-async def create_upload_file(
-    file: UploadFile = File(...), user: str = Depends(get_current_user)
-):
-    file_location = f"files/{file.filename}"
-    with open(file_location, "wb") as f:
+def create_upload_file(file: UploadFile = File(...), username: str = Depends(manager)):
+    file_location = f"./files/{file.filename}"
+    with open(file_location, "wb+") as f:
         f.write(file.file.read())
 
-    # Assume `process_csv` is your function to generate the PDF
     pdf_path = process_csv(file_location)
-
     return FileResponse(
         path=pdf_path, filename=os.path.basename(pdf_path), media_type="application/pdf"
     )
+
+
+@app.get("/")
+def home(username: str = Depends(manager)):
+    content = """
+        <body>
+            <form action='/uploadfile' enctype='multipart/form-data' method='post'>
+            <input name='file' type='file'>
+            <input type='submit'>
+            </form>
+        </body>
+    """
+    return HTMLResponse(content=content)
+
+
+@app.exception_handler(NotAuthenticatedException)
+def auth_exception_handler(request: Request, exc: NotAuthenticatedException):
+    """
+    Redirect the user to the login page if not logged in
+    """
+    return RedirectResponse(url="/login_form")
+
+
+@app.get("/login_form", response_class=HTMLResponse)
+def login_form():
+    return """
+    <!DOCTYPE html>
+    <html>
+       <body>
+          <form method="POST"  action="/login">
+             <label for="username">Username:</label><br>
+             <input type="text" id="username" name="username"><br>
+             <label for="password">Password:</label><br>
+             <input type="password" id="password" name="password"><br><br>
+             <input type="submit" value="Submit">
+          </form>
+       </body>
+    </html>
+    """
+
+
+# Main function to run the app
+if __name__ == "__main__":
+    import uvicorn
+
+    from process_csv import process_csv
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
--- process_csv.py
+++ process_csv.py
@@ -6,6 +6,41 @@ from reportlab.lib.pagesizes import A4
 from reportlab.lib.utils import ImageReader
 from reportlab.pdfgen import canvas
 
+IMC_ADULT = {
+    "18.5": "Bajo Peso",
+    "24.9": "Peso Normal",
+    "29.9": "Sobrepeso",
+    "34.9": "Obesidad Grado 1",
+    "39.9": "Obesidad Grado 2",
+    "1000": "Obesidad Grado 3",
+}
+IMC_OLD = {
+    "23": "Bajo Peso",
+    "27.9": "Peso Normal",
+    "31.9": "Sobrepeso",
+    "39.9": "Obesidad",
+    "1000": "Obesidad Morbida",
+}
+
+GRASA_VISCERAL = {"10": "Normal", "100": "Excesivo"}
+
+CC_MEN = {
+    "95": "Normal",
+    "102": "Riesgo Elevado",
+    "1000": "Riesgo Muy Elevado",
+}
+CC_WOMEN = {
+    "82": "Normal",
+    "88": "Riesgo Elevado",
+    "1000": "Riesgo Muy Elevado",
+}
+
+
+def get_classification(value: float, reference: dict) -> str:
+    for k in list(reference.keys()):
+        if value < float(k):
+            return reference[k]
+
 
 def draw_box(c, x, y, width, height, stroke=1):
     c.setLineWidth(stroke)
@@ -26,15 +61,22 @@ def generate_pdf(df, output_filename):
     c.setFont("Helvetica", 12)
     c.drawString(60, height - 90, f"Nombre: {df['Nombre'].iloc[0]}")
     c.drawString(60, height - 110, f"Genero: {df['Genero'].iloc[0]}")
-    c.drawString(60, height - 130, f"Fecha: {df['Fecha'].iloc[-1].date()}")
+    c.drawString(
+        60, height - 130, f"Fecha: {df['Fecha'].dt.strftime('%d/%m/%Y').iloc[-1]}"
+    )
     c.drawString(60, height - 150, f"Altura: {df['Altura'].iloc[0]} cm")
 
     # Evaluation Summary
-    box_height = 120
-    draw_box(c, 50, height - 300, width - 100, box_height)
+    box_height = 210
+    draw_box(c, 50, height - 390, width - 100, box_height)
     c.setFont("Helvetica-Bold", 12)
     c.drawString(60, height - 200, "Resumen de las Evaluaciones")
-    c.drawString(60, height - 220, "Indicador             Actual    Ultimo    General")
+    c.drawString(
+        60,
+        height - 220,
+        "Indicador                      Actual    Ultimo    General"
+        "     Clasificacion",
+    )
     c.setFont("Helvetica", 12)
     diff_weight = df["Peso Corporal (Kg)"].iloc[-1] - df["Peso Corporal (Kg)"].iloc[0]
     diff_weight = round(diff_weight, 1)
@@ -42,49 +84,112 @@ def generate_pdf(df, output_filename):
         df["Peso Corporal (Kg)"].iloc[-1] - df["Peso Corporal (Kg)"].iloc[-2]
     )
     diff_weight_last = round(diff_weight_last, 1)
+
     diff_muscle = df["Masa Muscular (Kg)"].iloc[-1] - df["Masa Muscular (Kg)"].iloc[0]
     diff_muscle = round(diff_muscle, 1)
     diff_muscle_last = (
         df["Masa Muscular (Kg)"].iloc[-1] - df["Masa Muscular (Kg)"].iloc[-2]
     )
+
     diff_muscle_last = round(diff_muscle_last, 1)
     diff_fat = df["Grasa(%)"].iloc[-1] - df["Grasa(%)"].iloc[0]
     diff_fat = round(diff_fat, 1)
     diff_fat_last = df["Grasa(%)"].iloc[-1] - df["Grasa(%)"].iloc[-2]
     diff_fat_last = round(diff_fat_last, 1)
+
+    diff_cc = df["CC"].iloc[-1] - df["CC"].iloc[0]
+    diff_cc = round(diff_cc, 1)
+    diff_cc_last = df["CC"].iloc[-1] - df["CC"].iloc[-2]
+    diff_cc_last = round(diff_cc_last, 1)
+    if df["Genero"].iloc[0] == "M":
+        cc_classification = get_classification(df["CC"].iloc[-1], CC_MEN)
+    else:
+        cc_classification = get_classification(df["CC"].iloc[-1], CC_WOMEN)
+
+    diff_cca = df["CCA"].iloc[-1] - df["CCA"].iloc[0]
+    diff_cca = round(diff_cca, 1)
+    diff_cca_last = df["CCA"].iloc[-1] - df["CCA"].iloc[-2]
+    diff_cca_last = round(diff_cca_last, 1)
+
+    diff_visceral_fat = df["GV"].iloc[-1] - df["GV"].iloc[0]
+    diff_visceral_fat = round(diff_visceral_fat, 1)
+    diff_visceral_fat_last = df["GV"].iloc[-1] - df["GV"].iloc[-2]
+    diff_visceral_fat_last = round(diff_visceral_fat_last, 1)
+    visceral_fat_classification = get_classification(df["GV"].iloc[-1], GRASA_VISCERAL)
+
+    diff_imc = df["IMC"].iloc[-1] - df["IMC"].iloc[0]
+    diff_imc = round(diff_imc, 1)
+    diff_imc_last = df["IMC"].iloc[-1] - df["IMC"].iloc[-2]
+    diff_imc_last = round(diff_imc_last, 1)
+    if df["Edad"].iloc[0] < 60:
+        imc_classification = get_classification(df["IMC"].iloc[-1], IMC_ADULT)
+    else:
+        imc_classification = get_classification(df["IMC"].iloc[-1], IMC_OLD)
+
+    diff_water = df["Agua (%)"].iloc[-1] - df["Agua (%)"].iloc[0]
+    diff_water = round(diff_water, 1)
+    diff_water_last = df["Agua (%)"].iloc[-1] - df["Agua (%)"].iloc[-2]
+    diff_water_last = round(diff_water_last, 1)
+
     c.drawString(
         60,
         height - 240,
-        f"Peso Corporal       {df['Peso Corporal (Kg)'].iloc[-1]}kg    "
+        f"Peso Corporal                     {df['Peso Corporal (Kg)'].iloc[-1]}kg    "
         f"{f'+{diff_weight_last}' if diff_weight_last > 0 else diff_weight_last}kg    "
         f"{f'+{diff_weight}' if diff_weight > 0 else diff_weight}kg",
     )
     c.drawString(
         60,
         height - 260,
-        f"Grasa (%)              {df['Grasa(%)'].iloc[-1]}%     "
+        f"Grasa (%)                          {df['Grasa(%)'].iloc[-1]}%     "
         f"{f'+{diff_fat_last}' if diff_fat_last > 0 else diff_fat_last}%     "
         f"{f'+{diff_fat}' if diff_fat > 0 else diff_fat}%",
     )
     c.drawString(
         60,
         height - 280,
-        f"Masa Muscular      {df['Masa Muscular (Kg)'].iloc[-1]}kg    "
+        f"Agua (%)                              {df['Agua (%)'].iloc[-1]}%     "
+        f"{f'+{diff_water_last}' if diff_water_last > 0 else diff_water_last}%     "
+        f"{f'+{diff_water}' if diff_water > 0 else diff_water}%",
+    )
+    c.drawString(
+        60,
+        height - 300,
+        f"Masa Muscular                      {df['Masa Muscular (Kg)'].iloc[-1]}kg    "
         f"{f'+{diff_muscle_last}' if diff_muscle_last > 0 else diff_muscle_last}kg    "
         f"{f'+{diff_muscle}' if diff_muscle > 0 else diff_muscle}kg",
     )
-
-    # Body Composition
-    box_height = 150
-    draw_box(c, 50, height - 460, width - 100, box_height)
-    c.setFont("Helvetica-Bold", 14)
-    c.drawString(60, height - 330, "Composición Corporal")
-    c.setFont("Helvetica", 12)
-    c.drawString(60, height - 350, f"Agua Corporal: {df['Agua (%)'].iloc[-1]}%")
-    c.drawString(60, height - 370, f"Grasa Visceral: {df['GV'].iloc[-1]}%")
-    c.drawString(60, height - 390, f"Masa Ósea: {df['MO'].iloc[-1]} kg")
-    c.drawString(60, height - 410, f"IMC: {df['IMC'].iloc[-1]}")
-    c.drawString(60, height - 430, f"Grasa Actual: {df['Grasa Actual (%)'].iloc[0]}%")
+    c.drawString(
+        60,
+        height - 320,
+        f"Circunferencia de Cintura          {df['CC'].iloc[-1]}cm    "
+        f"{f'+{diff_cc_last}' if diff_cc_last > 0 else diff_cc_last}cm    "
+        f"{f'+{diff_cc}' if diff_cc > 0 else diff_cc}cm",
+    )
+    c.drawString(
+        60,
+        height - 340,
+        f"Circunferencia de Cadera      {df['CCA'].iloc[-1]}cm    "
+        f"{f'+{diff_cca_last}' if diff_cca_last > 0 else diff_cca_last}cm    "
+        f"{f'+{diff_cca}' if diff_cca > 0 else diff_cca}cm"
+        f"            {cc_classification}",
+    )
+    c.drawString(
+        60,
+        height - 360,
+        f"Grasa Visceral(%)                   {df['GV'].iloc[-1]}%    "
+        f"{f'+{diff_visceral_fat_last}' if diff_visceral_fat_last > 0 else diff_visceral_fat_last}%    "
+        f"{f'+{diff_visceral_fat}' if diff_visceral_fat > 0 else diff_visceral_fat}%"
+        f"            {visceral_fat_classification}",
+    )
+    c.drawString(
+        60,
+        height - 380,
+        f"IMC                                 {df['IMC'].iloc[-1]}    "
+        f"{f'+{diff_imc_last}' if diff_imc_last > 0 else diff_imc_last}    "
+        f"{f'+{diff_imc}' if diff_imc > 0 else diff_imc}"
+        f"            {imc_classification}",
+    )
 
     # Add Graphs
     def add_plot_to_pdf(c, plot_func, x, y, width, height):
@@ -159,18 +264,16 @@ def generate_pdf(df, output_filename):
     graph_x = 50
 
     # Adjusted positions for multiple graphs
-    graph_y_grasa = height - 600
-    graph_y_peso = height - 600
-    graph_y_masa = height - 720
-    graph_y_pie = height - 720
-    graph_y_agua = height - 840
+    graph_y_grasa = height - 550
+    graph_y_peso = height - 550
+    graph_y_masa = height - 670
+    graph_y_pie = height - 700
+    graph_y_agua = height - 790
 
     add_plot_to_pdf(c, plot_grasa, graph_x, graph_y_grasa, graph_width, graph_height)
     add_plot_to_pdf(c, plot_peso, width - 300, graph_y_peso, graph_width, graph_height)
     add_plot_to_pdf(c, plot_masa, graph_x, graph_y_masa, graph_width, graph_height)
-    add_plot_to_pdf(
-        c, plot_pie, width - 240, graph_y_pie, graph_width - 80, graph_height
-    )
+    add_plot_to_pdf(c, plot_pie, width - 220, graph_y_pie, 150, 150)
     add_plot_to_pdf(c, plot_agua, graph_x, graph_y_agua, graph_width, graph_height)
 
     # Save the PDF
@@ -191,7 +294,9 @@ def process_csv(file_path):
         df["Masa Muscular (Kg)"].str.replace(",", ".").astype(float)
     )
     df["Masa Magra (%)"] = df["Masa Magra (%)"].str.replace(",", ".").astype(float)
+    df["Agua (%)"] = df["Agua (%)"].str.replace(",", ".").astype(float)
     df["Grasa(%)"] = df["Grasa(%)"].str.replace(",", ".").astype(float)
+    df["IMC"] = df["IMC"].str.replace(",", ".").astype(float)
 
     # Generate PDF for the entire CSV
     output_filename = "./informe.pdf"
